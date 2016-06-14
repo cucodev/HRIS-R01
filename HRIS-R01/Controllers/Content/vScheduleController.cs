51b6c9b72ebe1beac8b27a7112e3b051fe5c7c07
sofyan <sofyan@ABUSOFYANAF7D>
2016-06-14 19:39:54 +0700

    Add Calendar

---
+++ HRIS-R01/Controllers/Content/vScheduleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HRIS_R01.Controllers.Content
+{
+    public class vScheduleController : Controller
+    {
+        // GET: vSchedule
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
